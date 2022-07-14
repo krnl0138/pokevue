895cc9798e62f928a87dd707d4090d753ad125f8
krnl0138 <krnl03182@gmail.com>
2022-07-14 15:15:56 +0400

    add About page and AboutSection component
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

---
+++ components/aboutSection/AboutSection.tsx
@@ -0,0 +1,18 @@
+import { Heading } from "../heading/Heading";
+
+export const AboutSection = () => {
+  return (
+    <div>
+      <Heading title={"A fun way to find pokemons"} />
+      <p>
+        Laborum enim et cillum laboris eu fugiat ea magna magna velit Lorem
+        laborum irure Lorem. Duis elit nulla Lorem occaecat consequat ea do
+        reprehenderit cupidatat. Qui culpa ea velit occaecat consectetur elit ex
+        ut ad excepteur labore aute do. Exercitation cillum voluptate ea labore
+        qui tempor proident quis eiusmod dolor labore deserunt. Dolore sit
+        labore sint occaecat commodo do. Do magna commodo velit fugiat mollit
+        anim labore. Eu duis fugiat in consectetur dolor enim nostrud velit.
+      </p>
+    </div>
+  );
+};
---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AboutSection } from "../components/aboutSection/AboutSection";
+import { Layout } from "../components/layout/Layout";
+
+export const About = () => {
+  return (
+    <Layout>
+      <AboutSection />
+    </Layout>
+  );
+};
+
+export default About;
