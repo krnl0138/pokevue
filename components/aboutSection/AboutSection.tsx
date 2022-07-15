c35aa64ffca8372ec461b874c7bace34a1cbdf78
krnl0138 <krnl03182@gmail.com>
2022-07-15 21:41:28 +0400

    add info in About section
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- components/aboutSection/AboutSection.tsx
+++ components/aboutSection/AboutSection.tsx
@@ -5,13 +5,25 @@ export const AboutSection = () => {
     <div>
       <Heading title={"A fun way to find pokemons"} />
       <p>
-        I built this website just for fun as a pet-project of mine. Feel free to
-        use the source code on <a href="https://github.com/">Github</a>.
+        I built this website <s>just for fun</s> for my CV as a pet-project of
+        mine. Feel free to use the source code on{" "}
+        <a href="https://github.com/">Github</a>.
       </p>
       <br />
       <p>
         Before this project I&apos;ve never watched Pokemon series. So I myself
         learned a lot of new :)
+      </p>
+      <br />
+      <p>
+        For this project I've used the following stack:
+        <ul>
+          <li>MUI</li>
+          <li>React/Redux</li>
+          <li>NextJS</li>
+          <li>Typescript</li>
+          <li>Firebase</li>
+        </ul>
       </p>
     </div>
   );
