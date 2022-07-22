879e03ba0bafc9f590e69a759772bfc0c5d6ab18
krnl0138 <krnl03182@gmail.com>
2022-07-22 19:38:01 +0400

    refactor AboutSection with MUI components
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- components/aboutSection/AboutSection.tsx
+++ components/aboutSection/AboutSection.tsx
@@ -1,25 +1,30 @@
-import { Box } from "@mui/material";
-import { Heading } from "../utils/heading/Heading";
+import { Container, Typography } from "@mui/material";
+import Link from "next/link";
 
 export const AboutSection = () => {
   return (
-    <Box component="section">
-      <Heading title={"Welcome to Pokevue"} />
-      <p>
+    <Container component="section">
+      <Typography component="h1" variant="h1">
+        Welcome to Pokevue
+      </Typography>
+      <Typography component="p">
         I built this website <s>just for fun</s> for my CV as a pet-project of
-        mine. Feel free to use the source code on{" "}
-        <a href="https://github.com/">Github</a>.
-      </p>
+        mine. Feel free to see the source code on{" "}
+        <Link href="https://github.com/">
+          <a>Github</a>
+        </Link>
+        .
+      </Typography>
       <br />
-      <p>
+      <Typography component="p">
         Before this project I&apos;ve never watched Pokemon series. So I myself
         learned a lot of new :)
-      </p>
+      </Typography>
       <br />
-      <p>
+      <Typography component="p">
         For this project I&apos;ve used the following stack: MaterialUI +
         React/Redux + NextJS + Typescript + Firebase.
-      </p>
-    </Box>
+      </Typography>
+    </Container>
   );
 };
