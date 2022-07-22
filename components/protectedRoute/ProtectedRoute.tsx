78b6c5de9b6db0c6c4dde2800af3eaeb87f234b3
krnl0138 <krnl03182@gmail.com>
2022-07-22 19:41:59 +0400

    remove useEffects from ProtectedRoute
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- components/protectedRoute/ProtectedRoute.tsx
+++ components/protectedRoute/ProtectedRoute.tsx
@@ -1,10 +1,10 @@
 import { getAuth } from "firebase/auth";
-import { useEffect } from "react";
+import { useState } from "react";
 import { app, dbGetUser } from "../../database";
-import { PROJECT_URLS as urls } from "../../utils/constants";
+import { URLS } from "../../utils/constants";
 import { useRouter } from "next/router";
 import { useAppDispatch, useAppSelector } from "../../utils/hooks";
-import { Box, CircularProgress, Container, Typography } from "@mui/material";
+import { CircularProgress } from "@mui/material";
 import { getPokemon } from "../../lib/api/getPokemon";
 import {
   addFavouritePokemon,
@@ -19,59 +19,41 @@ export const ProtectedRoute = ({ children }: TProtectedRoute): JSX.Element => {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user);
-  const pokemons = useAppSelector((state) => state.pokemons.byId);
   const pokemonsIds = useAppSelector((state) => state.pokemons.allIds);
+  const [isFavsLoaded, setIsFavsLoaded] = useState(false);
 
   // Populate user in the global state if logged in or redirect to /login
-  useEffect(() => {
-    if (user.username) return;
+  if (!user.username) {
     const auth = getAuth(app);
     auth.onAuthStateChanged(async (user) => {
       if (user) {
         await dbGetUser();
       } else {
-        router.push(urls.login);
+        // router.push(URLS.login);
       }
     });
-    // TODO unsubscribe ?
-  }, [user.username, router, dispatch, user.favourites, pokemons]);
-
-  // Populate user's favourites in the global state
-  useEffect(() => {
-    if (!user.favourites) return;
-    const populateFavourites = async () => {
-      let result;
-      const favs = Object.values(user.favourites);
-      const duplicate = (id: number) => !pokemonsIds.includes(id);
-
-      if (!pokemonsIds) {
-        result = await Promise.all(
-          favs.map(async (id) => await getPokemon(id))
-        );
-      } else {
-        result = await Promise.all(
-          favs.filter(duplicate).map(async (id) => await getPokemon(id))
-        );
-      }
-
-      result.forEach((pok) => {
-        dispatch(addPokemon(pok));
-        dispatch(addFavouritePokemon(pok.id));
-      });
-    };
-    populateFavourites();
-  }, [dispatch, pokemonsIds, user.favourites]);
-
-  if (!user.username) {
-    return (
-      <Box maxWidth={"xl"} sx={{ backgroundColor: "primary.dark" }}>
-        <Container>
-          <CircularProgress />
-          <Typography>Loading...</Typography>
-        </Container>
-      </Box>
-    );
+    // wait until onAuthStateChanged
+    return <CircularProgress />;
   }
+
+  // check for duplicates already in the store
+  const favs = Object.values(user.favourites).filter(
+    (id) => !pokemonsIds?.includes(id)
+  );
+
+  const populateFavourites = async () => {
+    if (!favs) return;
+    setIsFavsLoaded(true);
+    const result = await Promise.all(
+      favs.map(async (id) => await getPokemon(id))
+    );
+
+    result.forEach((pok) => {
+      dispatch(addPokemon(pok));
+      dispatch(addFavouritePokemon(pok.id));
+    });
+  };
+  if (!isFavsLoaded) populateFavourites();
 
   return children;
 };
