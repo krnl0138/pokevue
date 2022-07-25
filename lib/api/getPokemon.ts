2d6c944d012c9479bc5cd240e21ce8a75d1dc8ef
krnl0138 <krnl03182@gmail.com>
2022-07-25 10:56:57 +0400

    add Promise.all in getPokemon
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- lib/api/getPokemon.ts
+++ lib/api/getPokemon.ts
@@ -1,3 +1,4 @@
+import { dbGetAverageRating } from "../../firebase/dbRatings";
 import { TPokemonResponse, TPokemonSpeciesResponse } from "../../utils/types";
 import { fetchPokemon } from "./fetchPokemon";
 import { fetchPokemonSpecies } from "./fetchPokemonSpecies";
@@ -5,12 +6,21 @@ import { parsePokemon } from "./parsePokemon";
 
 export const getPokemon = async (search: string | number) => {
   console.log("getPokemon was CALLED for id: ", search);
-  const pokemon = await fetchPokemon(search);
-  const pokemonSpecies = await fetchPokemonSpecies(search);
-
+  const [pokemon, pokemonSpecies] = await Promise.all([
+    fetchPokemon(search),
+    fetchPokemonSpecies(search),
+  ]);
+  console.log("pokemon from getPokemon is: ", pokemon);
   // TODO no await?
   // TODO should be different for each entitiy?
   const data = parsePokemon({ pokemon, pokemonSpecies });
+  console.log("pokemon from parsePokemon is: ", data);
+  const pokemonId = data.id;
+  console.log(
+    "dbGetAverageRating from getPokemon was called for id",
+    pokemonId
+  );
+  // TODO should it wait?
   const result = {
     ...data,
     isRandom: false,
