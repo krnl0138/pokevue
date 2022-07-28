f35da835ac37bdf42ca98cecf65df66b2cd3c835
krnl0138 <krnl03182@gmail.com>
2022-07-28 13:41:59 +0400

    refactor evoName parameter to a singly truthy value
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- lib/api/fetchEvolution.ts
+++ lib/api/fetchEvolution.ts
@@ -5,9 +5,10 @@ export async function fetchEvolution(search: number | string) {
   const result: TPokemon[] = [];
   const recursive = async (search: number | string) => {
     const pokemon = await getPokemon(search);
+    // const pokemon = getPokemonExperiment(search);
     result.push(pokemon);
     const evoName = pokemon.pokemonData.evolutionName;
-    if (evoName !== null && evoName !== undefined) {
+    if (evoName) {
       await recursive(evoName);
     } else {
       return;
