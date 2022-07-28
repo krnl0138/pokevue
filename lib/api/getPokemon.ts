efbe4ae1f3c15c2de3e16310079f05fd9a0ab712
krnl0138 <krnl03182@gmail.com>
2022-07-28 13:47:14 +0400

    remove unneeded test function, add new fields for comments, rating
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- lib/api/getPokemon.ts
+++ lib/api/getPokemon.ts
@@ -1,5 +1,3 @@
-import { dbGetAverageRating } from "../../firebase/dbRatings";
-import { TPokemonResponse, TPokemonSpeciesResponse } from "../../utils/types";
 import { fetchPokemon } from "./fetchPokemon";
 import { fetchPokemonSpecies } from "./fetchPokemonSpecies";
 import { parsePokemon } from "./parsePokemon";
@@ -10,38 +8,11 @@ export const getPokemon = async (search: string | number) => {
     fetchPokemon(search),
     fetchPokemonSpecies(search),
   ]);
-  console.log("pokemon from getPokemon is: ", pokemon);
-  // TODO no await?
-  // TODO should be different for each entitiy?
   const data = parsePokemon({ pokemon, pokemonSpecies });
-  console.log("pokemon from parsePokemon is: ", data);
-  const pokemonId = data.id;
-  console.log(
-    "dbGetAverageRating from getPokemon was called for id",
-    pokemonId
-  );
-  // TODO should it wait?
   const result = {
     ...data,
-    isRandom: false,
-    isFavourite: false,
-    isRecent: false,
-  };
-  return { ...result };
-};
-
-export const getPokemonTest = async (search: string | number) => {
-  // Offline test variant
-  const pokemonRes = await fetch(`/${search}.json`);
-  const pokemon: TPokemonResponse = await pokemonRes.json();
-  const pokemonSpeciesRes = await fetch("/pokemonSpeciesReturnedAPI.json");
-  const pokemonSpecies: TPokemonSpeciesResponse =
-    await pokemonSpeciesRes.json();
-
-  const data = parsePokemon({ pokemon, pokemonSpecies });
-
-  const result = {
-    ...data,
+    ratingAverage: -Infinity,
+    comments: [],
     isRandom: false,
     isFavourite: false,
     isRecent: false,
