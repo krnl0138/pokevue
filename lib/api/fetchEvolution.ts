ce4535cc63103ca56076b54b7b9c9268a0ab8a19
krnl0138 <krnl03182@gmail.com>
2022-07-31 11:15:43 +0400

    rewrite fetchEvo to get evo chain aside the first pokemon from the store
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- lib/api/fetchEvolution.ts
+++ lib/api/fetchEvolution.ts
@@ -1,19 +1,36 @@
 import { TPokemon } from "../../utils/types";
-import { getPokemon } from "./getPokemon";
+import { fetchPokemonSpecies } from "./fetchPokemonSpecies";
+import { retrievePokemon } from "./retrievePokemon";
 
+/*
+ * Just for the practise purpose we recursively fetch
+ * all pokemons aside the first one bc we it was fetched
+ * on the client-side.
+ * It is better and easier to SSR all evolution-chain.
+ */
 export async function fetchEvolution(search: number | string) {
   const result: TPokemon[] = [];
+  const first = await fetchPokemonSpecies(search);
+  const firstEvoName = first.evolves_from_species?.name;
+  if (!firstEvoName) return;
+
   const recursive = async (search: number | string) => {
-    const pokemon = await getPokemon(search);
-    // const pokemon = getPokemonExperiment(search);
+    console.log("search inside recursive is: ", search);
+    const pokemon = await retrievePokemon(search);
     result.push(pokemon);
     const evoName = pokemon.pokemonData.evolutionName;
-    if (evoName) {
-      await recursive(evoName);
-    } else {
+
+    // base case
+    if (!evoName) {
       return;
+    } else {
+      await recursive(evoName);
     }
   };
-  await recursive(search);
+  try {
+    await recursive(firstEvoName);
+  } catch (error) {
+    throw new Error("An error occured while fetching evolution pokemons");
+  }
   return result;
 }
