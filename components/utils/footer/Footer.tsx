8ee1d3a90330367ac4fc433d9b9f735e2b1930bd
krnl0138 <krnl03182@gmail.com>
2022-07-31 11:24:09 +0400

    footer css rewritten
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- components/utils/footer/Footer.tsx
+++ components/utils/footer/Footer.tsx
@@ -2,37 +2,61 @@ import { Box, List, ListItem, Typography } from "@mui/material";
 import Link from "next/link";
 import { URLS } from "../../../utils/constants";
 
+const styleFooterMain = {
+  width: "100%",
+  height: "7rem",
+  marginTop: "3rem",
+  marginBottom: "1rem",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "flex-start",
+  " p": {
+    fontWeight: "300",
+    letterSpacing: "0.1rem",
+    textTransform: "uppercase",
+  },
+  "#footer-title": {
+    fontWeight: 700,
+  },
+};
+
 export const Footer = () => {
   return (
-    <Box
-      component="footer"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        width: "100%",
-        height: "7rem",
-        p: { fontWeight: 700 },
-      }}
-    >
+    <Box component="footer" sx={styleFooterMain}>
       <Box component="nav" sx={{ marginBottom: 1 }}>
         <List sx={{ display: "flex" }}>
           <ListItem>
-            <Link href={URLS.home}>Home</Link>
+            <Typography component="p" variant="body1">
+              <Link href={URLS.home}>
+                <a>Home</a>
+              </Link>
+            </Typography>
           </ListItem>
           <ListItem>
-            <Link href={URLS.about}>About</Link>
+            <Typography component="p" variant="body1">
+              <Link href={URLS.about}>
+                <a>About</a>
+              </Link>
+            </Typography>
           </ListItem>
           <ListItem>
-            <Link href={URLS.profile}>Profile</Link>
+            <Typography component="p" variant="body1">
+              <Link href={URLS.profile}>
+                <a>Profile</a>
+              </Link>
+            </Typography>
           </ListItem>
           <ListItem>
-            <Link href={URLS.favourites}>Favourites</Link>
+            <Typography component="p" variant="body1">
+              <Link href={URLS.favourites}>
+                <a>Favourites</a>
+              </Link>
+            </Typography>
           </ListItem>
         </List>
       </Box>
-      <Typography component="p" variant="h6" sx={{ fontWeight: 700 }}>
+      <Typography id="footer-title" component="p" variant="h6">
         Pokevue, 2022
       </Typography>
     </Box>
