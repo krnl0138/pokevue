5bd86ffb032f61e6d22d910865ae01dc7bd16ca2
krnl0138 <krnl03182@gmail.com>
2022-07-31 11:25:23 +0400

    in Modal change Modal to Dialog component, move the ref container to the Layout section. Add styling to the Layout
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- components/utils/layout/Layout.tsx
+++ components/utils/layout/Layout.tsx
@@ -1,6 +1,37 @@
-import { Container } from "@mui/material";
+import { Box } from "@mui/material";
+import { POKEMON_LOGO } from "../../../utils/constants";
 import { Footer } from "../footer/Footer";
 import Header from "../header/Header";
+import { ModalCardWrapper } from "../modal/ModalCardWrapper";
+
+const styleLayoutMain = {
+  background:
+    "linear-gradient(211deg, rgba(255,255,255,1) 0%, rgba(0,0,0,0.02) 100%)",
+  height: "92vh",
+  overflow: "auto",
+  display: "flex",
+  flexDirection: "column",
+};
+
+const styleLayoutWrapper = {
+  flexGrow: 1,
+  position: "relative",
+  "::before": {
+    content: "''",
+    position: "fixed",
+    top: "15%",
+    right: "5%",
+    height: "200px",
+    width: "500px",
+    backgroundImage: `url(${POKEMON_LOGO})`,
+    backgroundSize: "500px 200px",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "top right",
+    filter: "opacity(.3)",
+    transform: "rotate(10deg)",
+    zIndex: "-1",
+  },
+};
 
 export const Layout = ({
   children,
@@ -10,12 +41,13 @@ export const Layout = ({
   return (
     <>
       <Header />
-      <main>
-        <Container>
+      <Box component="main" sx={styleLayoutMain}>
+        <Box component="div" sx={styleLayoutWrapper}>
           {children}
-          <Footer />
-        </Container>
-      </main>
+        </Box>
+        <ModalCardWrapper />
+        <Footer />
+      </Box>
     </>
   );
 };
--- components/utils/modal/ModalCardWrapper.tsx
+++ components/utils/modal/ModalCardWrapper.tsx
@@ -1,33 +1,27 @@
-import Modal from "@mui/material/Modal";
+import { Dialog } from "@mui/material";
 import React from "react";
 import {
   closeModal,
   selectModalData,
   selectModalStatus,
 } from "../../../lib/redux/slices/modalSlice";
-import { selectAllIds } from "../../../lib/redux/slices/pokemonsSlice";
 import { useAppDispatch, useAppSelector } from "../../../utils/hooks";
 import { PokemonCard } from "../../pokemonCards/pokemonCard/PokemonCard";
 
 export const ModalCardWrapper = () => {
   const dispatch = useAppDispatch();
-  const handleModalClose = () => {
-    dispatch(closeModal());
-  };
+  const handleModalClose = () => dispatch(closeModal());
   const isModalOpen = useAppSelector(selectModalStatus);
   const modalData = useAppSelector(selectModalData);
-  const allIds = useAppSelector(selectAllIds);
 
-  return (isModalOpen &&
-    typeof modalData === "number" &&
-    allIds.includes(modalData) && (
-      <Modal
-        open={isModalOpen}
-        onClose={handleModalClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <PokemonCard id={modalData} />
-      </Modal>
-    )) as JSX.Element;
+  return typeof modalData === "number" ? (
+    <Dialog
+      open={isModalOpen}
+      onClose={handleModalClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <PokemonCard id={modalData} inModal={true} />
+    </Dialog>
+  ) : null;
 };
