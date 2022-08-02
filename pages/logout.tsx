b0f7e34b733e48b8f4deff47427b9f7a892aacb5
krnl0138 <krnl03182@gmail.com>
2022-08-02 14:22:22 +0400

    add Logout component logic
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

---
+++ pages/logout.tsx
@@ -0,0 +1,30 @@
+import { getAuth, signOut } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { resetUser } from "../lib/redux/slices/userSlice";
+import { URLS } from "../utils/constants";
+import { useAppDispatch } from "../utils/hooks";
+
+const Logout = () => {
+  const router = useRouter();
+  const auth = getAuth();
+  const dispatch = useAppDispatch();
+
+  const handleRedirect = () => router.push(URLS.home);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        signOut(auth).then(() => {
+          dispatch(resetUser());
+          router.push(URLS.login);
+        });
+      } else {
+        handleRedirect();
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+};
+
+export default Logout;
